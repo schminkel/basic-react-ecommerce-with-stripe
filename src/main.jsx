a3f4d6902764b8eee06f320ce1bbf9c7a5d2d1f3
Thorsten Schminkel <thorsten@schminkel.de>
2022-12-23 18:45:07 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CartProvider
       mode="payment"
       cartMode="client-only"
-      stripe={import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY}
+      stripe={"pk_test_51KVz3lHxQ9v68qpG3VOVHvXoZ5gAMuGxDrJ8ICfuc9IkKrL0OE2p5iFYAL9MWfqKSXJ8IdCYcVLBTflfIQNDHXVT005mUdCINIe"}
       successUrl="https://schminkel.github.io/react-shopping-cart-client-only"
       cancelUrl="https://schminkel.github.io/react-shopping-cart-client-only"
       currency="EUR"

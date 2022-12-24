91e1fbd9e231711374d9e078ad82db20bf0cc5c5
Thorsten Schminkel <thorsten@schminkel.de>
2022-12-24 13:59:33 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+/**
+ * Renders the app
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider

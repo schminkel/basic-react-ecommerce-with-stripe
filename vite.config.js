cd73dfdbcbdd9a8863a3b3bd75d6af1e1899ee1d
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-02-11 10:38:52 +0100

    Update base path using environment variable in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-shopping-cart-client-only/",
+  base: import.meta.env.BASE_PATH,
   plugins: [react()],
 })

efb92cd933b8451645ea823f4656c9634a68c7eb
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-02-12 17:17:22 +0100

    Add dotenv for environment variable loading
    Installed dotenv library for handling environment variables in the project. Changes include adding dotenv into the package.json and package-lock.json files, and using dotenv.config() in the vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config(); // load env vars from .env file
 
 // This function is exported for Vite to use as configuration
 export default ({ mode }) => {
+
+  console.log("### (vite.config.js) process.env: ", process.env);
+
   // Load app-level env vars specific to the current mode
   const env = loadEnv(mode, process.cwd());
 

e7521dafb21b57fdd4095eec762d8d272787f289
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-02-12 11:20:24 +0100

    Move Stripe configuration to environment variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CartProvider } from 'use-shopping-cart'
+import {CartProvider} from 'use-shopping-cart'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -8,18 +8,18 @@ import './index.css'
  * Renders the app
  */
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CartProvider
-      mode="payment"
-      cartMode="client-only"
-      stripe="pk_test_51KVz3lHxQ9v68qpG3VOVHvXoZ5gAMuGxDrJ8ICfuc9IkKrL0OE2p5iFYAL9MWfqKSXJ8IdCYcVLBTflfIQNDHXVT005mUdCINI"
-      successUrl="https://schminkel.github.io/react-shopping-cart-client-only"
-      cancelUrl="https://schminkel.github.io/react-shopping-cart-client-only"
-      currency="EUR"
-      allowedCountries={['US', 'GB', 'DE']}
-      billingAddressCollection={true}
+    <React.StrictMode>
+      <CartProvider
+          mode="payment"
+          cartMode="client-only"
+          stripe={process.env.VITE_STRIPE_PUBLIC_KEY}
+          successUrl={process.env.VITE_STRIPE_SUCCESS_URL}
+          cancelUrl={process.env.VITE_STRIPE_CANCEL_URL}
+          currency="EUR"
+          allowedCountries={['US', 'GB', 'DE']}
+          billingAddressCollection={true}
       >
-      <App />
-    </CartProvider>
-  </React.StrictMode>,
+        <App/>
+      </CartProvider>
+    </React.StrictMode>,
 )

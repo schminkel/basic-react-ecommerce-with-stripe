0335e77048b21b1281f6a2bb6ad4ac0c6d3492c4
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-02-12 18:55:38 +0100

    Remove .env file and revise vite.config.js
    The revision involves deleting the .env file from the repository and updating vite.config.js to enhance the security. Now, environment variables are directly accessed through process.env, which prevents potential exposure of sensitive information.

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,26 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-dotenv.config(); // load env vars from .env file
+// load env vars from .env file or from the environment
+dotenv.config();
 
-// This function is exported for Vite to use as configuration
 export default ({ mode }) => {
-
   console.log("### (vite.config.js) process.env: ", process.env);
-
-  // Load app-level env vars specific to the current mode
-  const env = loadEnv(mode, process.cwd());
-
-  console.log("### (vite.config.js) mode: ", mode);
-  console.log("### (vite.config.js) Loaded env vars: ", env);
 
   return defineConfig({
     // Use the VITE_BASE_PATH environment variable, fallback to '/' if not defined
-    base: env.VITE_BASE_PATH || '/',
+    base: JSON.stringify(process.env.VITE_BASE_PATH) || '/',
 
     // Include plugins needed by the application, for example, react
     plugins: [react()],
 
     // Define global constants which can be replaced at build time, see main.jsx
     define: {
-      __VITE_BASE_PATH__: JSON.stringify(env.VITE_BASE_PATH),
-      __VITE_STRIPE_PUBLIC_KEY__: JSON.stringify(env.VITE_STRIPE_PUBLIC_KEY),
-      __VITE_STRIPE_SUCCESS_URL__: JSON.stringify(env.VITE_STRIPE_SUCCESS_URL),
-      __VITE_STRIPE_CANCEL_URL__: JSON.stringify(env.VITE_STRIPE_CANCEL_URL),
+      __VITE_BASE_PATH__: JSON.stringify(process.env.VITE_BASE_PATH) || '-',
+      __VITE_STRIPE_PUBLIC_KEY__: JSON.stringify(process.env.VITE_STRIPE_PUBLIC_KEY) || '-',
+      __VITE_STRIPE_SUCCESS_URL__: JSON.stringify(process.env.VITE_STRIPE_SUCCESS_URL) || '-',
+      __VITE_STRIPE_CANCEL_URL__: JSON.stringify(process.env.VITE_STRIPE_CANCEL_URL) || '-',
     },
   });
 };

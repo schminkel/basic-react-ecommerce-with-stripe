871727ecdb5e425ac0a1756d44e23ec67e7163ff
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-02-12 13:17:43 +0100

    Refactor environment variable handling in vite config and main.jsx
    This commit changes the way environment variables are accessed in both vite.config.js and main.jsx files. The method of 'loadEnv' from 'vite' is used to load environment variables in vite.config.js, while 'import.meta

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
-import { defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-console.log("### (vite.config.js) VITE_BASE_PATH: ", process.env.VITE_BASE_PATH);
+const env = loadEnv('', process.cwd(), '')
+console.log("### (vite.config.js) all env: ", env);
+console.log("### (vite.config.js) VITE_BASE_PATH: ", env.VITE_BASE_PATH);
+
+export default defineConfig({
+  base: env.VITE_BASE_PATH,
+  plugins: [react()],
+  define: {
+    __VITE_BASE_PATH__: JSON.stringify(env.VITE_BASE_PATH),
+
+  }
+})
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH,
-  plugins: [react()],
-})
+// export default defineConfig(({ command, mode }) => {
+//
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, process.cwd(), '')
+//
+//   console.log("### (vite.config.js) mode: ", mode);
+//   console.log("### (vite.config.js) env: ", env);
+//   console.log("### (vite.config.js) VITE_BASE_PATH: ", env.VITE_BASE_PATH);
+//
+//   return {
+//     // vite config
+//     define: {
+//       base: JSON.stringify(env.APP_ENV),
+//       plugins: [react()],
+//     },
+//
+//   }
+// })
